b82b62131f32d55d6fe8931f617ae0cee7bb0b80
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-08 17:14:51 -0400

    debut du script

---
+++ index.js
@@ -0,0 +1,8 @@
+//Declaration et initialisation de nos variables
+let height = document.getElementById("height-input");
+let weight = document.getElementById("weight-input");
+
+let btn = document.getElementById("btnBMI");
+
+let showResult = document.getElementById("result");
+let condition = document.getElementById("weightCondition-label");
