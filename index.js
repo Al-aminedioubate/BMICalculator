b3b4f1c0c747a95d7e22ce660ad481d81b784d8e
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-03-08 17:22:03 -0400

    calcul terminer

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //Declaration et initialisation de nos variables
-let height = document.getElementById("height-input");
-let weight = document.getElementById("weight-input");
+let inputHeight = document.getElementById("height-input");
+let inputWeight = document.getElementById("weight-input");
 
 let btn = document.getElementById("btnBMI");
 
 let showResult = document.getElementById("result");
 let condition = document.getElementById("weightCondition-label");
+
+//function permettant de faire le calcul du BMI(IMC)
+function calcuBMI(height, weight) {
+	let bmi = height / (weight * weight);
+}
